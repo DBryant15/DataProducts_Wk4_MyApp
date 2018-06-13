74706e23fa751001a38c5b50b70a49a06db6f876
DerekBryant <dwbryan@nppd.com>
2018-06-13 12:19:31 -0500

    20180613 1219 - DWB - Final commit.

--- DP_Wk4_DBryant/server.R
+++ DP_Wk4_DBryant/server.R
@@ -1,25 +1,35 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+#20180605 1306 - DWB - Not sure if this sqldf package is supported in Shiny.io.
+#I did check and this is a cran package so good chance it will.
+library(sqldf)
 
-# Define server logic required to draw a histogram
+# Define server logic required to print instructions and draw a plot
 shinyServer(function(input, output) {
-   
+  
+  #provides instructions for app
+  output$howTo <- renderPrint({
+    Text_of_HowTo <- "This app with display a barpolt of tree height through 25 years. The data starts as year 5 and ends at year 25. To see the growth at years 5, 10, 15,20, or 25: Click the Plot tab and slide the slider by left clicking your mouse on the slider."
+    print(Text_of_HowTo)
+  })
+  
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #loads a dataset for plotting
+    treesGrowing <- datasets::Loblolly
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    #SQL to get data for display
+    TreeHeightAtUserSelectedYear_QRY <- paste("SELECT Seed, height 
+                                     FROM treesGrowing
+                                     WHERE age = ", input$Years)
+    
+    #execution of SQL
+    treeHieghtAtYear <- sqldf::sqldf(TreeHeightAtUserSelectedYear_QRY)
+    
+    #create simple bar plot
+    barplot(treeHieghtAtYear$height, 
+            names.arg = treeHieghtAtYear$Seed,
+            ylim=c(0,70)
+            )
     
   })
   
--- DP_Wk4_DBryant/ui.R
+++ DP_Wk4_DBryant/ui.R
@@ -1,33 +1,30 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+  fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Tree height over time"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input to select the year of growth
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      sliderInput("Years",
+                  "Year of record:",
+                  min = 5,
+                  max = 25,
+                  value = 5,
+                  step = 5)
     ),
     
-    # Show a plot of the generated distribution
+    # tabs for instruction and display of plot
     mainPanel(
-       plotOutput("distPlot")
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("distPlot")), 
+                  tabPanel("HowTo", verbatimTextOutput("howTo"))
     )
   )
-))
+)
+)
+)
